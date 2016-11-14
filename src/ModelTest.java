829e5131176f64e73d9689ff3ab0554540f7b16c
toyinodujebe <toyinodujebe@cmail.carleton.ca>
2016-11-14 18:24:16 -0500

    Added test cases

---
+++ src/ModelTest.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ModelTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSub() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDiv() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMul() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetResult() {
+		fail("Not yet implemented");
+	}
+
+}
